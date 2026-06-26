60bc46e72a50c3d9995a28d7e22f1c3d55d339b1
Anek Green <movepoint@internet.ru>
2026-06-26 12:10:12 +0500

    tests pass

--- lib/ocaml_rest.ml
+++ lib/ocaml_rest.ml
@@ -11,6 +11,7 @@ open Angstrom
 
 let text (p : string t) = lift (fun cnt -> Text cnt) p
 let em (p : string t) = char '*' *> lift (fun cnt -> Em cnt) p <* char '*'
+let strong (p : string t) = string "**" *> lift (fun cnt -> Strong cnt) p <* string "**"
 let para (p : markup list t) = lift (fun cnt -> Para cnt) p
 let nonspaces : string t = take_while1 (function | ' ' | '\t' -> false | _ -> true)
 let spaces : string t = take_while1 (function | ' ' | '\t' -> true | _ -> false)
@@ -18,7 +19,7 @@ let spaces : string t = take_while1 (function | ' ' | '\t' -> true | _ -> false)
 let plain_string : string t = take_while1 (function | '_' | '*' -> false | _ -> true)
 (*let markupable : string t = lift3 (fun n1 a n2 -> n1 ^ a ^ n2) nonspaces plain_string nonspaces*)
 let markupable = plain_string
-let contents = many (text plain_string <|> em markupable)
+let contents = many (text plain_string <|> em markupable <|> strong markupable)
 let parse (str:string) : rest =
   match parse_string ~consume:All (para contents) str with
   | Ok v      -> v
